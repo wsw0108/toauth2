c54d9fca71b247955559c4724e273a53d1e6fae0
wsw0108 <wsw0108@gmail.com>
2021-05-10 17:16:39 +0800

    fill OpenID for qq.User

--- qq/oauth2.go
+++ qq/oauth2.go
@@ -26,15 +26,15 @@ var (
 )
 
 type User struct {
-	OpenID    string `json:"openid"`
-	Nickname  string `json:"nickname"`
-	Gender    string `json:"gender"`
-	AvatarURL string `json:"figureurl_qq"`
-	Email     string `json:"email"`
-}
-
-type meJSON struct {
-	OpenID string `json:"openid"`
+	OpenID       string
+	Email        string
+	Nickname     string
+	FigureURL    string
+	FigureURL1   string
+	FigureURL2   string
+	FigureURLqq1 string
+	FigureURLqq2 string
+	Gender       string
 }
 
 func AuthCodeURL(c *oauth2.Config, state string) string {
@@ -92,6 +92,10 @@ func Exchange(ctx context.Context, c *oauth2.Config, code string) (*oauth2.Token
 	return token, nil
 }
 
+type meJSON struct {
+	OpenID string `json:"openid"`
+}
+
 func GetOpenID(ctx context.Context, token *oauth2.Token) (string, error) {
 	var buf bytes.Buffer
 	buf.WriteString(OpenIDURL)
@@ -124,6 +128,18 @@ func GetOpenID(ctx context.Context, token *oauth2.Token) (string, error) {
 	return me.OpenID, err
 }
 
+type userJSON struct {
+	Ret          int    `json:"ret"`
+	Msg          string `json:"msg"`
+	Nickname     string `json:"nickname"`
+	FigureURL    string `json:"figureurl"`      // 大小为30×30像素的QQ空间头像URL
+	FigureURL1   string `json:"figureurl_1"`    // 大小为50×50像素的QQ空间头像URL
+	FigureURL2   string `json:"figureurl_2"`    // 大小为100×100像素的QQ空间头像URL
+	FigureURLqq1 string `json:"figureurl_qq_1"` // 大小为40×40像素的QQ头像URL(一定会有)
+	FigureURLqq2 string `json:"figureurl_qq_2"` // 大小为100×100像素的QQ头像URL
+	Gender       string `json:"gender"`
+}
+
 func GetUser(ctx context.Context, c *oauth2.Config, token *oauth2.Token) (*User, error) {
 	openID, err := GetOpenID(ctx, token)
 	if err != nil {
@@ -135,6 +151,7 @@ func GetUser(ctx context.Context, c *oauth2.Config, token *oauth2.Token) (*User,
 	v.Set("access_token", token.AccessToken)
 	v.Set("oauth_consumer_key", c.ClientID)
 	v.Set("openid", openID)
+	v.Set("format", "json")
 	if strings.Contains(UserInfoURL, "?") {
 		buf.WriteByte('&')
 	} else {
@@ -150,10 +167,23 @@ func GetUser(ctx context.Context, c *oauth2.Config, token *oauth2.Token) (*User,
 		return nil, err
 	}
 	defer resp.Body.Close()
-	var user User
-	err = json.NewDecoder(resp.Body).Decode(&user)
+	var uj userJSON
+	err = json.NewDecoder(resp.Body).Decode(&uj)
 	if err != nil {
 		return nil, err
+	}
+	if uj.Ret < 0 {
+		return nil, errors.New(uj.Msg)
+	}
+	user := User{
+		OpenID:       openID,
+		Nickname:     uj.Nickname,
+		FigureURL:    uj.FigureURL,
+		FigureURL1:   uj.FigureURL1,
+		FigureURL2:   uj.FigureURL2,
+		FigureURLqq1: uj.FigureURLqq1,
+		FigureURLqq2: uj.FigureURLqq2,
+		Gender:       uj.Gender,
 	}
 	return &user, nil
 }
